4e379e680ddfd7663d0194bce1655ace126c1a59
mritunjay <mritunjaypandey0789@gmail.com>
2025-12-16 18:25:39 +0530

    favicon error fix

--- portfolio_builder/urls.py
+++ portfolio_builder/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
 ]
 
 # Serve media files in development
